988e8c5dd0283c4a1bdef046ebedb44af91e266d
Kynnet <110259702+Kynnet@users.noreply.github.com>
2023-10-11 19:15:47 -0700

    fixed math again again + input params

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/DepositMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/DepositMath.java
@@ -6,9 +6,9 @@ import org.firstinspires.ftc.teamcode.utils.Pose2d;
 import org.firstinspires.ftc.teamcode.utils.Vector3;
 
 public class DepositMath {
-    private final double v4BarLength = 10;
+    public final double v4BarLength = 40;
     private final Vector3 slidePos = new Vector3(5,0,10);
-    private final double slideAngle = Math.toRadians(60);
+    public final double slideAngle = Math.toRadians(60);
     private final double slideMax = 100;
 
     public double slideExtension;
@@ -19,12 +19,13 @@ public class DepositMath {
     public DepositMath() {
     }
 
-    public void calculate(double xError, double yError, double heading, double height, double yOffset) {
-        double relX = xError*Math.cos(heading) + (yError+yOffset)*Math.sin(heading);
-        double relY = -xError*Math.sin(heading) + (yError+yOffset)*Math.cos(heading);
+    public void calculate(double xError, double yError, double headingError, double slideHeight, double yOffset) {
+        xError += slideHeight * Math.cos(slideAngle); //im p sure this should work --kyle
+        double relX = xError*Math.cos(headingError) + (yError+yOffset)*Math.sin(headingError);
+        double relY = -xError*Math.sin(headingError) + (yError+yOffset)*Math.cos(headingError);
         Vector3 slideUnit = new Vector3(Math.cos(slideAngle),0,Math.sin(slideAngle));
 
-        Vector3 depositPos = new Vector3(relX - slidePos.x, relY - slidePos.y, height - slidePos.z);
+        Vector3 depositPos = new Vector3(relX - slidePos.x, relY - slidePos.y, slideHeight - slidePos.z);
         Vector3 slideProject = Vector3.project(depositPos, slideUnit);
         Vector3 remainder = Vector3.subtract(depositPos, slideProject);
 
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/DepositMathChecker.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode.tests;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.subsystems.deposit.DepositMath;
+import org.firstinspires.ftc.teamcode.utils.TelemetryUtil;
+import org.firstinspires.ftc.teamcode.utils.Vector3;
+
+@TeleOp
+@Config
+public class DepositMathChecker extends LinearOpMode {
+    public static double xError = 10;
+    public static double yError = 0;
+    public static double headingError = 0;
+    public static double height = 10;
+    public static double yOffset = 0;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot robot = new Robot(hardwareMap);
+        DepositMath depositMath = new DepositMath();
+        waitForStart();
+
+        while (!isStopRequested()) {
+            depositMath.calculate(xError,yError,headingError,height,yOffset);
+            Vector3 calcPos = new Vector3(0,0,0);
+            double relX = depositMath.slideExtension * Math.cos(depositMath.slideAngle) + depositMath.v4BarLength * Math.cos(depositMath.v4BarYaw);
+            double relY = depositMath.v4BarLength * Math.sin(depositMath.v4BarYaw);
+            double relZ = depositMath.slideExtension * Math.sin(depositMath.slideAngle) + depositMath.v4BarLength * Math.sin(depositMath.v4BarPitch);
+
+            calcPos.x = relX * Math.cos(headingError) - relY * Math.sin(headingError);
+            calcPos.y = relX * Math.sin(headingError) + relY * Math.cos(headingError);
+            calcPos.z = relZ;
+            TelemetryUtil.packet.put("posx", calcPos.x);
+            TelemetryUtil.packet.put("posy", calcPos.y);
+            TelemetryUtil.packet.put("posz", calcPos.z);
+        }
+    }
+}
