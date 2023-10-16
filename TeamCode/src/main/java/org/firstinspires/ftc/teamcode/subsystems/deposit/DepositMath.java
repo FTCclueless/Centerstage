fc02c7de259cb8841671a149175d519d566ec7ab
dinomasater186 <peterwetherell186@gmail.com>
2023-10-16 15:46:03 -0600

    Fixed DepositMath because Kyle was kinda goofy

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/DepositMath.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/DepositMath.java
@@ -9,6 +9,7 @@ public class DepositMath {
     public final double v4BarLength = 40;
     private final Vector3 slidePos = new Vector3(0,0,0);
     public final double slideAngle = Math.toRadians(60);
+    private final Vector3 slideUnit = new Vector3(Math.cos(slideAngle),0,Math.sin(slideAngle));
     private final double slideMax = 100;
 
     public double slideExtension;
@@ -20,37 +21,30 @@ public class DepositMath {
     }
 
     public void calculate(double xError, double yError, double headingError, double slideHeight, double yOffset) {
-        xError += slideHeight * Math.cos(slideAngle); //im p sure this should work --kyle
-        double relX = xError*Math.cos(headingError) + (yError+yOffset)*Math.sin(headingError);
-        double relY = -xError*Math.sin(headingError) + (yError+yOffset)*Math.cos(headingError);
-        Vector3 slideUnit = new Vector3(Math.cos(slideAngle),0,Math.sin(slideAngle));
+        xError += slideHeight * Math.cos(slideAngle);
+        yError += yOffset;
+        double relX = xError*Math.cos(headingError) + yError*Math.sin(headingError);
+        double relY = -xError*Math.sin(headingError) + yError*Math.cos(headingError);
 
         Vector3 depositPos = new Vector3(relX - slidePos.x, relY - slidePos.y, slideHeight - slidePos.z);
         Vector3 slideProject = Vector3.project(depositPos, slideUnit);
         Vector3 remainder = Vector3.subtract(depositPos, slideProject);
 
+        slideExtension = slideProject.getMag();
         if (Math.abs(remainder.getMag()) > v4BarLength) {
-            slideExtension = slideProject.getMag();
             Log.e("deposit out of range!!!", "e");
         }
-        else if (remainder.getMag() == v4BarLength) {
-            slideExtension = slideProject.getMag();
-        }
         else {
+            double extra = Math.sqrt(Math.pow(v4BarLength, 2) - Math.pow(remainder.getMag(), 2));
+            slideExtension -= extra;
             remainder = Vector3.subtract(depositPos, Vector3.mul(slideUnit, slideExtension));
-            double extra = Math.sqrt(Math.pow(v4BarLength,2) - Math.pow(remainder.getMag(),2));
-            if (slideProject.getMag() + extra < slideMax) {
-                slideExtension = slideProject.getMag() + extra;
-            }
-            else {
-                slideExtension = slideProject.getMag() - extra;
-            }
         }
         if (slideExtension > slideMax || slideExtension < 0) {
             Log.e("slide out of range", slideExtension + "");
+            slideExtension = Math.min(Math.max(slideExtension,0),slideMax);
         }
         v4BarYaw = Math.atan2(remainder.y,remainder.x);
-        v4BarPitch = Math.atan2(remainder.x, Math.sqrt(Math.pow(remainder.x,2) + Math.pow(remainder.y,2)));
+        v4BarPitch = Math.atan2(remainder.z, Math.sqrt(Math.pow(remainder.x,2) + Math.pow(remainder.y,2)));
     }
 
 
