e2a835f1300d478eeba035b4d51a23c9b39698ac
Kynnet <110259702+Kynnet@users.noreply.github.com>
2023-11-03 21:06:40 -0700

    tester done for mech

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/yoinkers/SlidesMotorYoinker.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.tests.yoinkers;
+
+import android.util.Log;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.firstinspires.ftc.robotcore.external.navigation.CurrentUnit;
+import org.firstinspires.ftc.teamcode.Robot;
+import org.firstinspires.ftc.teamcode.utils.TelemetryUtil;
+
+@Config
+@TeleOp
+public class SlidesMotorYoinker extends LinearOpMode {
+    @Override
+    public void runOpMode() throws InterruptedException {
+        Robot robot = new Robot(hardwareMap);
+
+        DcMotorEx slide1 = hardwareMap.get(DcMotorEx.class, "slide1");
+        DcMotorEx slide2 = hardwareMap.get(DcMotorEx.class, "slide2");
+        slide1.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slide1.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        slide2.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        slide2.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        slide1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+
+        waitForStart();
+
+        while (!isStopRequested()) {
+            TelemetryUtil.packet.put("current1", slide1.getCurrent(CurrentUnit.MILLIAMPS));
+            TelemetryUtil.packet.put("current2", slide2.getCurrent(CurrentUnit.MILLIAMPS));
+
+            Log.e("current1: ", ""+slide1.getCurrent(CurrentUnit.MILLIAMPS));
+            Log.e("current2: ", "" + slide2.getCurrent(CurrentUnit.MILLIAMPS));
+            robot.update();
+        }
+    }
+}
