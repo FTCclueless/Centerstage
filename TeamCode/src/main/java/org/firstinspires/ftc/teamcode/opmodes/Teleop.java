0bb79743369226db6b2343e4ffd7abefd327b6d9
martine <martin0esparragoza@gmail.com>
2023-11-05 17:49:49 -0800

    I HATE WRITING TELEOPS

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/Teleop.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.opmodes;
 
+import android.widget.Button;
+
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
@@ -7,98 +9,80 @@ import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.utils.ButtonToggle;
 import org.firstinspires.ftc.teamcode.utils.Globals;
 import org.firstinspires.ftc.teamcode.utils.RunMode;
+import org.firstinspires.ftc.teamcode.utils.Vector2;
+import org.firstinspires.ftc.teamcode.utils.Vector3;
 
 @TeleOp
 public class Teleop extends LinearOpMode {
-    enum State {
-        READY,
-        INTAKE,
-        DEPOSIT,
-        DUNK;
-
-        public static State nextState(State state) {
-            switch (state) {
-                case READY:
-                    return INTAKE;
-                case INTAKE:
-                    return DEPOSIT;
-                case DEPOSIT:
-                    return DUNK;
-                case DUNK:
-                    return READY;
-            }
-            return READY;
-        }
-    };
-
-    private State state = State.READY;
-
     @Override
     public void runOpMode() {
         Robot robot = new Robot(hardwareMap);
         Globals.RUNMODE = RunMode.TELEOP;
-        ButtonToggle btnX =      new ButtonToggle();
-        ButtonToggle btnB =      new ButtonToggle();
-        ButtonToggle btnA =      new ButtonToggle();
-        ButtonToggle btnY =      new ButtonToggle();
-        ButtonToggle dpadUp =    new ButtonToggle();
-        ButtonToggle dpadDown =  new ButtonToggle();
-        ButtonToggle dpadLeft =  new ButtonToggle();
-        ButtonToggle dpadRight = new ButtonToggle();
+        ButtonToggle btDpadUp =       new ButtonToggle();
+        ButtonToggle btDpadDown =     new ButtonToggle();
+        ButtonToggle btDpadLeft =     new ButtonToggle();
+        ButtonToggle btDpadRight =    new ButtonToggle();
+        ButtonToggle btLeftTrigger =  new ButtonToggle();
+        ButtonToggle btLeftBumper =   new ButtonToggle();
+        ButtonToggle btRightTrigger = new ButtonToggle();
+        ButtonToggle btRightBumper =  new ButtonToggle();
+        ButtonToggle btY =            new ButtonToggle();
+        ButtonToggle btA =            new ButtonToggle();
+        ButtonToggle btX =            new ButtonToggle();
+        ButtonToggle btB =            new ButtonToggle();
+        Vector3 depoPos =             new Vector3(0, 0, 0); /* I messed up the coord systems so badly :( */
 
         waitForStart();
 
         while (!isStopRequested()) {
-            switch (state) {
-                case READY:
-                    break;
+            // Weemawaeoeoewa :)
+            if (btRightBumper.isClicked(gamepad1.right_bumper)) {
+                robot.intake.setActuationPixelHeight((int) (robot.intake.getIntakeActuationOffset() + 1));
+            }
+            if (btRightTrigger.isClicked(gamepad1.right_trigger > 0.2)) {
+                robot.intake.setActuationPixelHeight((int) (robot.intake.getIntakeActuationOffset() - 1));
+            }
 
-                case INTAKE:
-                    if (btnB.isToggled(gamepad1.b)) {
-                        robot.intake.on();
-                    } else {
-                        robot.intake.off();
-                    }
+            boolean depoFlag = false;
+            if (btDpadUp.isClicked(gamepad1.dpad_up)) {
+                depoFlag = true;
+                depoPos.y++;
+            }
+            if (btDpadDown.isClicked(gamepad1.dpad_down)) {
+                depoFlag = true;
+                depoPos.y--;
+            }
+            if (btDpadLeft.isClicked(gamepad1.dpad_left)) {
+                depoFlag = true;
+                depoPos.x--;
+            }
+            if (btDpadRight.isClicked(gamepad1.dpad_right)) {
+                depoFlag = true;
+                depoPos.x++;
+            }
 
-                    if (gamepad1.a) { // TODO: Make a ButtonToggle
-                        robot.intake.setActuationPixelHeight(5);
-                    }
-                    if (gamepad1.y) {
-                        robot.intake.setActuationPixelHeight(1);
-                    }
-                    break;
+            if (btLeftBumper.isClicked(gamepad1.left_bumper)) {
+                depoFlag = true;
+                depoPos.z++;
+            }
+            if (btLeftTrigger.isClicked(gamepad1.left_trigger > 0.2)) {
+                depoFlag = true;
+                depoPos.z--;
+            }
 
-                case DEPOSIT:
-                    // use depositAt in order to get the thing done
+            if (depoFlag) {
+                robot.deposit.depositAt(depoPos.y, depoPos.x, depoPos.z);
+            }
 
-                    //if (dpadUp.isClicked(gamepad1.dpad_up))
-                    //if (dpadDown.isClicked(gamepad1.dpad_down)
-                    //if (dpadLeft.isClicked(gamepad1.dpad_left)
-                    //if (dpadRight.isClicked(gamepad1.dpad_right)
+            if (btX.isClicked(gamepad1.x)) {
+                robot.intake.toggle();
+            }
 
-                    if (gamepad1.a) {
-                        // Offset forward
-                    }
-                    if (gamepad1.y) {
-                        // Offset backward
-                    }
-
-                    break;
-
-                case DUNK:
-                    // if (btnA.isClicked(gamepad1.a)) dunk
-
-                    if (/* pixel count 0 */ false)
-                        // Reset deposit offset
-                        state = State.READY;
-
-                    break;
+            if (btB.isClicked(gamepad1.b)) {
+                robot.deposit.dunk(1);
             }
 
             robot.drivetrain.drive(gamepad1);
-            if (btnX.isClicked(gamepad1.x)) // This might brick during DUNK to ready -- Eric
-                state = State.nextState(state);
-
             robot.update();
         }
     }
