7bd73fb5ecae0aad33120ea7bae5b75ade4b42aa
hkim <hudson.n.kim@gmail.com>
2024-04-01 00:59:22 -0700

    minor tele change

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/Deposit.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/deposit/Deposit.java
@@ -36,13 +36,13 @@ public class Deposit {
     double targetX = 0.0;
 
     // v4bar angles
-    public static double v4BarTransferAngle = -0.302586780420;
-    public static double v4BarGrabAngle = -0.0504311300;
+    public static double v4BarTransferAngle = -0.296983;
+    public static double v4BarGrabAngle = -0.1456899;
     public static double v4BarDepositAngle = -3.2556096189;
 
     // top servo angles
-    public static double topServoTransferAngle = -0.9357776357448;
-    public static double topServoGrabAngle = -0.8349153756;
+    public static double topServoTransferAngle = -1.07020;
+    public static double topServoGrabAngle = -1.0309803;
     public static double topServoDepositAngle = 2.017245202803;
     public static double topServoRetractAngle = 0.7060358;
 
