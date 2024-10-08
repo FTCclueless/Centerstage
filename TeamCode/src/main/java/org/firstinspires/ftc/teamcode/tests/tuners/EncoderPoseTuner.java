2988b3bf6f3e0a7bcc8d69ef994217c1951a77bd
mura2432 <houcheng.jg@gmail.com>
2024-10-08 16:59:07 -0700

    Fixed EncoderPoseTuner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/tuners/EncoderPoseTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tests/tuners/EncoderPoseTuner.java
@@ -7,10 +7,16 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.Robot;
 import org.firstinspires.ftc.teamcode.sensors.Sensors;
 import org.firstinspires.ftc.teamcode.subsystems.drive.Drivetrain;
+import org.firstinspires.ftc.teamcode.subsystems.drive.localizers.Localizer;
+import org.firstinspires.ftc.teamcode.utils.Encoder;
+import org.firstinspires.ftc.teamcode.utils.Pose2d;
 
 @TeleOp
 public class EncoderPoseTuner extends LinearOpMode {
     @Override
+
+    private Encoder[] encoders;
+
     public void runOpMode() throws InterruptedException {
         Robot robot = new Robot(hardwareMap);
         Drivetrain drivetrain = robot.drivetrain;
@@ -20,22 +26,36 @@ public class EncoderPoseTuner extends LinearOpMode {
         double wheelRadius = 0.984252; //for 50mm wheels
         double ticksToInches = (wheelRadius * Math.PI * 2.0) / ticksPerRotation;
 
-        int[] odometry;
+        encoders = new Encoder[3];
+
+        encoders[0] = new Encoder(new Pose2d(0,7.233361867),  -1); // left
+        encoders[1] = new Encoder(new Pose2d(0,-6.7290861),-1); // right
+        encoders[2] = new Encoder(new Pose2d(-9.018771197, 0),  -1); // back
+
+        int[]odometry;
         double theta;
 
         waitForStart();
 
+
+
         while (!isStopRequested()) {
             drivetrain.drive(gamepad1);
             odometry = sensors.getOdometry();
+            updateEncoders(odometry);
             theta = Math.PI * 20; // 10 rotations
 
             robot.update();
 
-            telemetry.addData("leftOdoRadius", (odometry[0]*ticksToInches)/theta + "");
-            telemetry.addData("rightOdoRadius", (odometry[1]*ticksToInches)/theta + "");
-            telemetry.addData("backOdoRadius", (odometry[2]*ticksToInches)/theta + "");
+            telemetry.addData("leftOdoRadius", encoders[0].getCurrentDist()/theta + "");
+            telemetry.addData("rightOdoRadius", encoders[1].getCurrentDist()/theta + "");
+            telemetry.addData("backOdoRadius", encoders[2].getCurrentDist()/theta + "");
             telemetry.update();
+        }
+    }
+    public void updateEncoders(int[] encoders) {
+        for (int i = 0; i < this.encoders.length; i ++){
+            this.encoders[i].update(encoders[i]);
         }
     }
 }
